6c66cc9bbad5767e19fe23bc80f0f4c92214c06b
0knowledge888 <gregmouret@gmail.com>
2025-11-07 17:38:50 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "FinTales App",
+  description: "Organisez, analysez et racontez vos aventures financières avec Fintales. Recevez des insights personnalisés et des récits captivants basés sur vos données financières.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { JSX } from "react";
+
+const Home: () => JSX.Element = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default Home;
